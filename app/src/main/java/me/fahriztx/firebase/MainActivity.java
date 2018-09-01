2b079332184122044778eeb81b949bc22d4e7af6
Muhammad Fahri <fahriztx77@gmail.com>
2018-09-01 12:31:57 +0700

    initial commit

--- app/src/main/java/me/fahriztx/firebase/MainActivity.java
+++ app/src/main/java/me/fahriztx/firebase/MainActivity.java
@@ -1,13 +1,75 @@
 package me.fahriztx.firebase;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.email) EditText email;
+    @BindView(R.id.password) EditText password;
+    @BindView(R.id.btnLogin) Button btnLogin;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void onBtnLoginClick(View v){
+        String Email = email.getText().toString();
+        String Password = password.getText().toString();
+        Pattern VALID_EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher match = VALID_EMAIL_REGEX.matcher(Email);
+
+        if(!match.find()){
+            email.setError("Format email salah!");
+            email.requestFocus();
+        }else if(TextUtils.isEmpty(Email)){
+            email.setError("Email tidak boleh kosong!");
+            email.requestFocus();
+        }else if(TextUtils.isEmpty(Password)){
+            password.setError("Password tidak boleh kosong!");
+            password.requestFocus();
+        }else{
+            auth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        FirebaseUser user = auth.getCurrentUser();
+
+                        if(user != null){
+                            Toast.makeText(MainActivity.this, user.getUid(), Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this, "Login Gagal", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
